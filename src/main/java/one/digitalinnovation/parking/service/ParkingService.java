ca09cf5c96762d949165c53106713c8349ba18c1
André Felipe <afmcmorais@icloud.com>
2022-08-16 18:23:01 -0300

    Primeiro endpoint de lista

---
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -0,0 +1,25 @@
+package one.digitalinnovation.parking.service;
+
+import one.digitalinnovation.parking.model.Parking;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class ParkingService {
+
+    private static Map<String, Parking> parkingMap = new HashMap();
+    static {
+        var id = getUUID();
+        Parking parking = new Parking(id, "ABC-1234", "DF", "VW Gol", "Branco");
+        parkingMap.put(id, parking);
+    }
+
+    public List<Parking> findAll(){
+        return parkingMap.values().stream().collect(Collectors.toList());
+    };
+    private static String getUUID() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+}
